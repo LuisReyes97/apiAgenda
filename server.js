d6a7b127a12ea0c6c5ff9676769819f21569b0d0
Luis Reyes <59459778+LuisReyes97@users.noreply.github.com>
2021-12-16 21:59:36 -0600

    Luis Reyes

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express')
+const mysql = requiere('mysql')
+const myconn = require('express-myconnection')
 
+//configuraciones e inicializaciones
 const app = express()
-app.set('port', process.env.PORT || 9000 )
+app.set('port', process.env.PORT || 9000)
+const dbAgenda = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'agenda'
+}
 
+//middleware
+app.use(myconn(mysql, dbAgenda, 'single'))
+
+// rutas
 app.get('/',(req, res) =>{
     res.send('corriendo')
 })
 
-app.listen(9000, ()=>{
-    console.log('server running on port',9000)
-} )
+//iniciando servidor
+
+app.listen(app.get('port'), ()=>{
+    console.log('server running on port',app.get('port'))
+})
+
